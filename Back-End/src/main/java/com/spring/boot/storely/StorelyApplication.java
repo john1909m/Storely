d2f1375bd682333476d94fabbb76d8f90206413f
john1909m <johnemil21@yahoo.com>
2026-01-15 23:13:19 +0200

    building the base of the project files

---
+++ Back-End/src/main/java/com/spring/boot/storely/StorelyApplication.java
@@ -0,0 +1,13 @@
+package com.spring.boot.storely;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StorelyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StorelyApplication.class, args);
+	}
+
+}
---
+++ Back-End/src/test/java/com/spring/boot/storely/StorelyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.boot.storely;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StorelyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
