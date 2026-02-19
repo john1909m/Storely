3d89870a563528ac01b1162f5a4f6775f5608278
john1909m <johnemil21@yahoo.com>
2026-02-19 13:53:06 +0200

    edit cors

--- Back-End/src/main/java/com/spring/boot/config/SecurityConfig.java
+++ Back-End/src/main/java/com/spring/boot/config/SecurityConfig.java
@@ -72,7 +72,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5173","http://localhost:8080","https://www.storely-eg.com")); // React frontend
+        configuration.setAllowedOrigins(List.of("http://localhost:5173","http://localhost:8080","https://www.storely-eg.com","https://storely-eg.com")); // React frontend
         configuration.setAllowedMethods(List.of("GET","POST","PUT","DELETE","OPTIONS")) ;
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
