b18b5bfc50421eec465923d06090600cf5aa77bd
john1909m <johnemil21@yahoo.com>
2026-03-19 22:39:52 +0200

    fix visits 2

--- Front-End/src/pages/vendor/VendorStore.jsx
+++ Front-End/src/pages/vendor/VendorStore.jsx
@@ -397,7 +397,7 @@ const VendorStore = () => {
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
             </svg>
             <span className="text-sm text-gray-600">Total visits:</span>
-            <span className="font-semibold text-gray-900">{Math.round(store.totalVisits / 2) || 0}</span>
+            <span className="font-semibold text-gray-900">{Math.ceil(store.totalVisits / 2) || 0}</span>
           </div>
         </div>
         {/* Simple Store URL Bar */}
