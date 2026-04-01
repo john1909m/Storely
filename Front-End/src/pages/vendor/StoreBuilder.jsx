087a2676059843c0bc89774f2e88464c017bbb97
john1909m <johnemil21@yahoo.com>
2026-04-02 00:48:47 +0200

    builder v4

--- Front-End/src/pages/vendor/StoreBuilder.jsx
+++ Front-End/src/pages/vendor/StoreBuilder.jsx
@@ -16,12 +16,14 @@ import { useErrorHandler } from '../../hooks/useErrorHandler';
 import StoreFooter from '../../components/StoreFooter';
 import Toast from '../../components/ui/Toast';
 // import PageToggles from '../../components/vendor/StoreBuilder/PageToggles';
-import SectionWrapper from '../../components/vendor/StoreBuilder/SectionWrapper';
-import BannerSection from '../../components/vendor/StoreBuilder/sections/BannerSection';
+// import SectionWrapper from '../../components/vendor/StoreBuilder/SectionWrapper';
+// import BannerSection from '../../components/vendor/StoreBuilder/sections/BannerSection';
 import FeaturedProductsSection from '../../components/vendor/StoreBuilder/sections/FeaturedProductsSection';
 import CategoriesSection from '../../components/vendor/StoreBuilder/sections/CategoriesSection';
 import FooterSection from '../../components/vendor/StoreBuilder/sections/FooterSection';
 import PageToggles from './../../components/vendor/storeBuilder/PageToggles';
+import SectionWrapper from './../../components/vendor/storeBuilder/SectionWrapper';
+import BannerSection from './../../components/vendor/storeBuilder/sections/BannerSection';
 
 const StoreBuilder = () => {
   const navigate = useNavigate();
